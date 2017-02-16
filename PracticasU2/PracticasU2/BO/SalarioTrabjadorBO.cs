363237114c7d480803ff449de73c541f94140f15
Daniel Pat <danielpat6@gmail.com>
2017-02-15 20:10:38 -0600

    Inicio del ejercicio 1

---
+++ PracticasU2/PracticasU2/BO/SalarioTrabjadorBO.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticasU2.BO
+{
+    class SalarioTrabjadorBO
+    {
+        private double salarioMensual;
+        private double incentivo;
+        private double porcentaje;
+        private double bonificacion;
+        private double salarioNeto;
+
+        public double SalarioMensual
+        {
+            get
+            {
+                return salarioMensual;
+            }
+
+            set
+            {
+                salarioMensual = value;
+            }
+        }
+
+        public double Incentivo
+        {
+            get
+            {
+                return incentivo;
+            }
+
+            set
+            {
+                incentivo = value;
+            }
+        }
+
+        public double Porcentaje
+        {
+            get
+            {
+                return porcentaje;
+            }
+
+            set
+            {
+                porcentaje = value;
+            }
+        }
+
+        public double CalcularSalarioNeto()
+        {
+            bonificacion = (salarioMensual * porcentaje) / 100;
+            salarioMensual = salarioMensual + incentivo + bonificacion;
+            return salarioMensual;
+        }
+
+    }
+}
---
+++ PracticasU2/PracticasU2/GUI/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace PracticasU2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Random d = new Random();
+            MessageBox.Show( d.Next(1, 5).ToString());
+
+        }
+    }
+}

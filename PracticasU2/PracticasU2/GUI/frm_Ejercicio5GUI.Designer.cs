138861bee0810343b5565180b97ec74e14d9ed42
Daniel Pat <danielpat6@gmail.com>
2017-02-25 13:57:21 -0600

    Listo ejercicio 5

--- PracticasU2/PracticasU2/GUI/frm_Ejercicio5GUI.Designer.cs
+++ PracticasU2/PracticasU2/GUI/frm_Ejercicio5GUI.Designer.cs
@@ -32,6 +32,8 @@
             this.lst_LibroCompra = new System.Windows.Forms.ListBox();
             this.btn_Agregar = new System.Windows.Forms.Button();
             this.btn_Quitar = new System.Windows.Forms.Button();
+            this.lbl_Libros = new System.Windows.Forms.Label();
+            this.lbl_Librocompra = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lst_Libros
@@ -83,11 +85,33 @@
             this.btn_Quitar.UseVisualStyleBackColor = true;
             this.btn_Quitar.Click += new System.EventHandler(this.btn_Quitar_Click);
             // 
+            // lbl_Libros
+            // 
+            this.lbl_Libros.AutoSize = true;
+            this.lbl_Libros.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Libros.Location = new System.Drawing.Point(21, 12);
+            this.lbl_Libros.Name = "lbl_Libros";
+            this.lbl_Libros.Size = new System.Drawing.Size(49, 18);
+            this.lbl_Libros.TabIndex = 4;
+            this.lbl_Libros.Text = "Libros";
+            // 
+            // lbl_Librocompra
+            // 
+            this.lbl_Librocompra.AutoSize = true;
+            this.lbl_Librocompra.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Librocompra.Location = new System.Drawing.Point(384, 9);
+            this.lbl_Librocompra.Name = "lbl_Librocompra";
+            this.lbl_Librocompra.Size = new System.Drawing.Size(135, 18);
+            this.lbl_Librocompra.TabIndex = 5;
+            this.lbl_Librocompra.Text = "Libros por comprar";
+            // 
             // frm_Ejercicio5GUI
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(624, 261);
+            this.Controls.Add(this.lbl_Librocompra);
+            this.Controls.Add(this.lbl_Libros);
             this.Controls.Add(this.btn_Quitar);
             this.Controls.Add(this.btn_Agregar);
             this.Controls.Add(this.lst_LibroCompra);
@@ -95,6 +119,7 @@
             this.Name = "frm_Ejercicio5GUI";
             this.Text = "Libros";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -104,5 +129,7 @@
         private System.Windows.Forms.ListBox lst_LibroCompra;
         private System.Windows.Forms.Button btn_Agregar;
         private System.Windows.Forms.Button btn_Quitar;
+        private System.Windows.Forms.Label lbl_Libros;
+        private System.Windows.Forms.Label lbl_Librocompra;
     }
 }
---
+++ PracticasU2/PracticasU2/GUI/frm_Ejercicio7GUI.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PracticasU2.GUI
+{
+    public partial class frm_Ejercicio7GUI : Form
+    {
+        public frm_Ejercicio7GUI()
+        {
+            InitializeComponent();
+        }
+    }
+}
